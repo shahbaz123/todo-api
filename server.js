2675e9ff0545eae3cf4d3cfa73d14ad44b9b17d3
Shah Akhter <shahroha5@gmail.com>
2017-05-26 23:17:39 -0700

    todos update

--- server.js
+++ server.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id:1,
+	description:'Meet Mom for lunch',
+	completed:false
+},{
+	id:2,
+	description:'Go to Market',
+	completed:false
+},{
+	id:3,
+	description:'Work is done!',
+	completed:true
+}]
 
 app.get('/', function(req , res){
 	res.send('Todo API Root');
+});
+
+app.get('/todos' , function(req , res){
+	res.json(todos);
+});
+
+app.get('/todos/:id' , function(req , res){
+	var todoId = parseInt(req.params.id , 10);
+	var matchTodo;
+    /*for ( var i = todos.length - 1; i >= 0; i--) {
+    	if ( i == req.params.id)
+    		res.json(todos[i]);
+    }*/
+    todos.forEach (function (todo){
+    	if (todoId == todo.id)
+    		matchTodo = todo;
+    });
+    if (matchTodo)
+       res.json(matchTodo);
+    res.status(404).send();
 });
 
 app.listen(PORT, function(){
